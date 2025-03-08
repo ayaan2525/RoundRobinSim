321dbbac99f0198b6583c74a666dd5139fcdcfa5
ayaan2525 <ayaan.siddiqui2525@gmail.com>
2025-03-08 17:30:30 +0100

    added scheduler script

---
+++ schuduler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+generate_tasks() {
+    num_tasks=$1
+    echo "PID,BurstTime,ArrivalTime" > tasks.csv
+    for ((i=1; i<=num_tasksl i++)); do
+        burst_time=$((RANDOM % 10 + 1)) #1-10 sec
+        arrival_time=$((RANDOM % 5)) #0-4 sec
+        echo "$,i,$burst_time,$arrival_time" >> tasks.csv
+    done
+}
+
+if [ "$#" -ne 2]; then
+    echo "Usage: bash scheduler.sh <num_tasks> <time_quantum>"
+    exit 1
+fi
+
+generate_tasks $1
+python3 scheduler.py tasks.csv $2 # execute python scheduler
